0b38afad9d7e3800bb870d0bb2d8d8643536c872
Amritpal Singh <amritpal@singh.email>
2016-03-26 15:31:57 -0400

    corrected bug in Stack.hpp, it was using BSTree macro

--- Stack.hpp
+++ Stack.hpp
@@ -1,5 +1,5 @@
-#ifndef BSTREE_CLASS_H
-#define BSTREE_CLASS_H
+#ifndef STACK_CLASS_H
+#define STACK_CLASS_H
 
 #include <vector>
 #include <iostream>
@@ -11,7 +11,7 @@ class Stack
 		~Stack();
 		void push(int element);
 		int pop(int *ptr);
-		
+
 	private:
 		int nmbItems;
 		vector<int> StackVector;
